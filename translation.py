b8a38cf5fc2ae0f5e11afd9931cc129f5a9cd388
alexanderfreitag <ge78zil@mytum.de>
2024-06-23 18:30:42 +0200

    add translation logic

--- translation.py
+++ translation.py
@@ -1,3 +1,23 @@
-from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
-model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-de-en")
+# Load the fine-tuned model and tokenizer
+model_de = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-de-en")
+tokenizer_de = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-de-en")
+model_bg = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-bg-en")
+tokenizer_bg = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-bg-en")
+
+# Create a translation pipeline
+translation_pipeline_de_en = pipeline("translation_de_to_en", model=model_de, tokenizer=tokenizer_de)
+translation_pipeline_bg_en = pipeline("translation_bg_to_en", model=model_bg, tokenizer=tokenizer_bg)
+
+
+# Function to translate German text to English
+def translate_german_to_english(german_text):
+    translated_text = translation_pipeline_de_en(german_text)
+    return translated_text[0]['translation_text']
+
+
+# Function to translate Bulgarian text to English
+def translate_bulgarian_to_english(bulgarian_text):
+    translated_text = translation_pipeline_bg_en(bulgarian_text)
+    return translated_text[0]['translation_text']
--- user_config.py
+++ user_config.py
@@ -1,2 +1,2 @@
-TYPE = "all-to-en-tags" # Choose one of "embeddings", "all-to-en", "all-to-all", "all-to-en-tags"
+TYPE = "all-to-en-tags"  # Choose one of "embeddings", "all-to-en", "all-to-all", "all-to-en-tags"
 OLLAMA_MODELFILE = None
